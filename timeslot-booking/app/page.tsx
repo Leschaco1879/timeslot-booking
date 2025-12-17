36931bb49cdbd218b7b4073479816a2719a2e59a
Leschaco1879 <Kevin.Stedtnitz@leschaco.com>
2025-12-17 09:02:45 +0100

    Create page.tsx

---
+++ timeslot-booking/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Lager Timeslot Buchung</h1>
+      <ul>
+        <li>
+          <a href="/book">➡️ Zur Buchung</a>
+        </li>
+        <li>
+          <a href="/admin">➡️ Admin Bereich</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
