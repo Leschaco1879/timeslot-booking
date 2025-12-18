ff3810f438b61bcd81f4fffc9dc45086933d7fc3
Leschaco1879 <Kevin.Stedtnitz@leschaco.com>
2025-12-18 15:41:14 +0100

    Update page.tsx

--- timeslot-booking/app/page.tsx
+++ timeslot-booking/app/page.tsx
@@ -3,12 +3,8 @@ export default function Home() {
     <div style={{ padding: 40 }}>
       <h1>Lager Timeslot Buchung</h1>
       <ul>
-        <li>
-          <a href="/book">➡️ Zur Buchung</a>
-        </li>
-        <li>
-          <a href="/admin">➡️ Admin Bereich</a>
-        </li>
+        <li><a href="/book">➡️ Zur Buchung</a></li>
+        <li><a href="/admin">➡️ Admin Bereich</a></li>
       </ul>
     </div>
   );
